5648598b66b07c2a7b42b00869f87b985c16699a
just8 <just8@mail.ru>
2019-04-23 11:54:24 +0300

    Дополнительные оптимизации функционала календаря.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,28 +9,53 @@
  */
 
 angular.module('calendarApp')
-  .controller('MainCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
+    .controller('MainCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
 
-    var vm = this;
+        let vm = this,
+            _counter;
 
-    // Получить значение сегодняшнего дня.
-    vm.today = function() {
-      return vm.today = $filter('date')(new Date(), 'dd');
-    }
+        // Получаем значение текущего месяца. Присваиваем его переменной счетчика.
+        _counter = parseInt($filter('date')(new Date(), 'MM'));
+
+        // Получить данные, для рендиренга сетки календаря.
+        let date = () => {
+            $http({
+                method: 'GET',
+                url: 'list/list' + _counter + '.json'
+            }).then(function (data) {
+                vm.calendar = data.data;
+                vm.dateToday = data.data[0].dateToday;
+
+            }, function (error) {
+            });
+        }
+
+        // Получить значения сегодняшнего дня и месяца. Сделать переключение на месяц с сегодняшней датой, если был выбран другой месяц.
+        vm.getToday = () => {
+            _counter = parseInt($filter('date')(new Date(), 'MM'));
+            date();
+            return vm.today = $filter('date')(new Date(), 'MM-dd');
+        }
+
+        // Функция переключающая календарь на 1 месяц вперед. Тестово сделано только до мая.
+        vm.next = () => {
+            _counter < 5 ? _counter++ : _counter = 5;
+            date();
+            return _counter;
+        }
+
+        // Функция переключающая календарь на 1 месяц назад. Тестово сделано только до марта.
+        vm.prev = () => {
+            _counter > 3 ? _counter-- : _counter = 3;
+            date();
+            return _counter;
+        }
+
+        // При загрузке страницы, вызываем функцию рендиренга сетки календаря.
+        date();
+
+        // Дополнительное свойство, можно использовать для заголовка результатов поиска.
+        vm.resultSearch = 'Результаты поиска';
 
 
-    // Получить данные, для рендиренга сетки календаря.
-    $http({
-      method: 'GET',
-      url: 'list/list.json'
-    }).then(function (data){
-      vm.calendar = data.data;
-
-    },function (error){
-    });
-
-    // Дополнительное свойство, можно использовать для заголовка результатов поиска.
-    vm.resultSearch = 'Результаты поиска';
-
-
-  }]);
+    }]);
